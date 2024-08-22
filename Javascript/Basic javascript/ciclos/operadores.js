728975e9c80367c57160f5b231bb0b6db9182981
Daniel Novillo <82913698+Danthology96@users.noreply.github.com>
2024-08-22 13:37:05 -0500

    ✨ Functions module

---
+++ Javascript/Basic javascript/ciclos/operadores.js
@@ -0,0 +1,20 @@
+/*
+
+Operadores de comparación
+
+*/
+
+// ==  hace comparación de valor
+// === hace comparación de valor y tipo 
+// !=  hace diferencia de valor
+// !== hace diferencia de valor y tipo
+// > mayor que
+// <  menor que
+// >= mayor o igual que
+// <= menor o igual que
+
+const a = 10;
+const b = 20;
+const c = "10"
+
+a == b; 
---
+++ Javascript/Basic javascript/condicionales/condicional if/if.js
@@ -0,0 +1,9 @@
+let nombre = "Daniel";
+
+if (nombre === "Pedro") {
+    console.log("Hola Pedro");
+} else if (nombre === "Daniel") {
+    console.log("Hola Daniel");
+} else {
+    console.log("Nombre no encontrado");
+}
